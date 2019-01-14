390f7c6ee7c2f7d7f30c09db0bc84f226a1de238
Marta Kandova <kmartaaaa1989@gmail.com>
2019-01-14 21:29:36 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,6 +7,8 @@ function updatePlaylist(playlist, artist, song) {
 }
 
 function removeFromPlaylist(playlist, artist) {
+  console.log("~~~~~~~~", playlist.artist)
+  console.log("~~~~~++++++~~~~~~+++++", playlist[artist]);
   delete playlist[artist];
   return playlist;
 }
